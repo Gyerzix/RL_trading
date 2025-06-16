d817bd44ec1089cc00fc6ee952bbe36d054fcaa6
shakh <shahingadgiev@gmail.com>
2025-06-16 16:33:04 +0300

    almost final version

--- pkl_open.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-# Открываем файл в бинарном режиме ('rb' - read binary)
-with open('trajectories.pkl', 'rb') as file:
-    data = pickle.load(file)
-
-print(f"Total bags: {len(data)}")
-print("First bag return:", data[0]["return"])
-print("Returns min/max:", min(d["return"] for d in data), max(d["return"] for d in data))
